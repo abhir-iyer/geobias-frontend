24d0bb19526d130b5b2ebaad76c73076745a3b9c
Abhir Iyer <98158477+abhir-iyer@users.noreply.github.com>
2025-05-22 18:48:36 -0400

    Update Heatmap.js

--- src/components/Heatmap.js
+++ src/components/Heatmap.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import Plot from 'react-plotly.js';
 
-export default function Heatmap({ data }) {
+export default function Heatmap({ data, layoutProps = {}, configProps = {} }) {
+  if (!data || data.length === 0) return null;
+
   const pivot = {};
   data.forEach(row => {
     const src = row.source_country;
@@ -16,24 +18,31 @@ export default function Heatmap({ data }) {
   const z = sources.map(src => targets.map(tgt => pivot[src]?.[tgt] ?? null));
 
   return (
-    <Plot
-      data={[{
-        z,
-        x: targets,
-        y: sources,
-        type: 'heatmap',
-        colorscale: 'RdBu',
-        reversescale: true,
-        zmid: 0,
-      }]}
-      layout={{
-        title: '',
-        width: 900,
-        height: 600
-      }}
-      config={{
-        responsive: true
-      }}
-    />
+    <div style={{ width: '100%', height: 'auto' }}>
+      <Plot
+        data={[{
+          z,
+          x: targets,
+          y: sources,
+          type: 'heatmap',
+          colorscale: 'RdBu',
+          reversescale: true,
+          zmid: 0,
+        }]}
+        layout={{
+          ...layoutProps,
+          margin: { t: 30, b: 40, l: 60, r: 10 },
+          height: 500,
+          paper_bgcolor: '#fff',
+          plot_bgcolor: '#fff'
+        }}
+        config={{
+          displayModeBar: false,
+          scrollZoom: false,
+          ...configProps
+        }}
+        style={{ width: '100%', height: '100%' }}
+      />
+    </div>
   );
 }
